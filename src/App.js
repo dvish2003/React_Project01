8c2b0453c009621528a680db6fe29168304bfe92
vishmee <Vishanchathuranga2003@gmail.com>
2025-02-11 01:58:14 +0530

    React Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PartOne from "./componets/PartOne";
+
+function App() {
+  return (
+    <div className="App">
+     <h1>This My First React Project</h1>
+        <PartOne/>
+    </div>
+  );
+}
+
+export default App;
