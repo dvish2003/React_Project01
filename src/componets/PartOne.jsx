e657ac82b43959fb0f1770f7325ff8b582bc5cdb
vishmee <Vishanchathuranga2003@gmail.com>
2025-02-11 02:03:55 +0530

    First react File

--- src/componets/PartOne.jsx
+++ src/componets/PartOne.jsx
@@ -1,1 +1,10 @@
-react
+import React from 'react';
+
+export default function PartOne() {
+    return (
+        <div>
+            <h1>Part One</h1>
+            <p>This is the first part of the application.</p>
+        </div>
+    )
+}
