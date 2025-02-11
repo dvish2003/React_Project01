42de4026deca1c01e98920accda49c3d28a066cb
vishmee <Vishanchathuranga2003@gmail.com>
2025-02-11 22:43:33 +0530

    React

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,16 @@ export default function Home() {
     return (
         <div>
             <NavBar/>
-            <TopicBox/>
+            {/*use One Prpos*/
+            /*<TopicBox food="Kottu"/>
+            <TopicBox food="Rice"/>
+            <TopicBox food="Hopppers"/>*/}
+
+            {/*use multi props*/}
+            <TopicBox food="Kottu" price="Rs 250"/>
+            <TopicBox food="Rice" price="Rs 304"/>
+            <TopicBox food="Hopppers" price="Rs 450"/>
+
         </div>
     )
 }
--- src/componets/TopicBox.jsx
+++ src/componets/TopicBox.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import './topicBox.css'
 
+/*
 export default function TopicBox() {
     const topic = "My Favourite Topic is Java"; //dynamic components
     const Item = "And PHP"; //dynamic components
     return (
         <div>
 <div className="topicBox">
-  {/*  <span>
+  {/!*  <span>
         This is A Topic
         //Manual Components
-    </span>*/}
+    </span>*!/}
 
-  {/*  <span>
+  {/!*  <span>
         {topic} {Item}
          Dynamic Components
-    </span>*/}
-   {/* <span>
+    </span>*!/}
+   {/!* <span>
         {alert("Hello Vishan")}
         //this is a alert message
-    </span>*/}
+    </span>*!/}
 </div>
+        </div>
+    )
+}
+*/
+
+//Use Properties
+export default function TopicBox(props) {
+
+    return (
+        <div>
+            <div className="topicBox">
+<span className="text">
+    My Favourite food is {props.food} and the price is {props.price}.
+</span>
+            </div>
         </div>
     )
 }
